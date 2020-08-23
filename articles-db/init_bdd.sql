3e1c1b8aeb2051057887e9302bd1bc0de132a4e9
JeremYD <delpech.jeremy@hotmail.fr>
2020-08-23 23:15:35 +0200

    Init Angular

--- articles-db/init_bdd.sql
+++ articles-db/init_bdd.sql
@@ -1,4 +1,4 @@
-create table article (
+create table article CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci (
     id int not null auto_increment,
     libelle varchar(100) unique not null,
     prix decimal(6, 2) not null,
